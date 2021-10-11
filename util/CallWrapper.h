ea06562524f71f05e59910c647e33e2fd069acae
jgernandt <92187948+jgernandt@users.noreply.github.com>
2021-10-11 22:14:44 +0200

    Add files via upload

---
+++ util/CallWrapper.h
@@ -0,0 +1,56 @@
+//Copyright 2021 Jonas Gernandt
+//
+//This file is part of VFX Editor.
+//
+//VFX Editor is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//VFX Editor is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with VFX Editor. If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+#include <functional>
+
+namespace util
+{
+	//Wrap a future call in an exception-safe manner.
+	//Note: std::bind may itself throw, if a constructor of any of the decayed types throws. But when would that happen?
+	template<typename... Args>
+	class CallWrapper
+	{
+	public:
+		CallWrapper() {}
+
+		//Disallow copying
+		CallWrapper(const CallWrapper&) = delete;
+		CallWrapper& operator=(const CallWrapper&) = delete;
+
+		//Allow moving
+		CallWrapper(CallWrapper&& other)
+		{
+			m_call.swap(other.m_call);
+		}
+		CallWrapper& operator=(CallWrapper&& other)
+		{
+			m_call = std::move(other.m_call);
+			return *this;
+		}
+
+		template<typename call_type>
+		CallWrapper(call_type call, Args&&... args) : m_call{ std::bind(call, std::forward<Args>(args)...) } {}
+
+		~CallWrapper() { if (m_call) m_call(); }
+
+		explicit operator bool() const { return static_cast<bool>(m_call); }
+
+	private:
+		std::function<void()> m_call;
+	};
+}
---
+++ util/Timer.h
@@ -0,0 +1,39 @@
+//Copyright 2021 Jonas Gernandt
+//
+//This file is part of VFX Editor.
+//
+//VFX Editor is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//VFX Editor is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with VFX Editor. If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <chrono>
+
+template<typename tick_t = long long, typename period_t = std::nano>
+class Timer
+{
+private:
+	using clock_t = std::chrono::steady_clock;
+	using duration_t = std::chrono::duration<tick_t, period_t>;
+	using time_t = std::chrono::time_point<clock_t>;
+public:
+	Timer()	{ m_startTime = clock_t::now(); }
+	~Timer() {}
+
+	tick_t elapsed() const { return std::chrono::duration_cast<duration_t>(clock_t::now() - m_startTime).count();	}
+	void reset() { m_startTime = clock_t::now(); }
+
+private:
+
+	time_t m_startTime;
+};
