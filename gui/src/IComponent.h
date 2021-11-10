074bc59a69128fbe090d6edb3b62c73daec929b0
jgernandt <jonas.gernandt@gmail.com>
2021-11-10 03:50:32 +0100

    Added selection state functions (experimental)

--- gui/src/IComponent.h
+++ gui/src/IComponent.h
@@ -88,6 +88,10 @@ namespace gui
 		virtual IComponent* getRoot() = 0;
 
 		virtual void setMouseHandler(MouseHandler*) = 0;
+
+		//Are these general enough to belong here?
+		virtual void setFocussed(bool) = 0;
+		virtual void setSelected(bool) = 0;
 	};
 
 	typedef std::unique_ptr<IComponent> ComponentPtr;
